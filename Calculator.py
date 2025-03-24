c00245265ccafae54c18e528512b6ef212551fba
Noblesom <160488466+Noblesom@users.noreply.github.com>
2025-03-24 23:34:53 +0100

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+# Simple Calculator project with python
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+operation = input("Enter operation(+,-,*,/,//) :")
+
+if operation == "+":
+    print(f"Result: {num1 + num2}")
+elif operation =="-":
+        print(f"Result: {num1 - num2}")
+elif operation =="*":
+        print(f"Result: {num1 * num2}")
+elif operation == "//":
+        print(f"Results: {num1 // num2}")
+elif operation =="/":
+     if num2 !=0:
+            print(f"Result:{num1 / num2}")
+     else:
+            print("Error: Division by Zero is not allowed.")
+else:
+        print("Invalid operation")
